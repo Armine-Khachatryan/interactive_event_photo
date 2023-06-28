476b863ee837d7c4865b86617b43c090d99430b1
elsaeucalyptuslabs <elsa@eucalyptuslabs.com>
2023-06-28 11:15:51 +0400

    test

--- src/app/components/TaskModal/TaskModal.js
+++ src/app/components/TaskModal/TaskModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Close from '../../assets/images/Close.svg';
 import Modal from 'react-modal';
 import './TaskModal.css'
@@ -6,14 +6,14 @@ import {NewUploader} from "../NewUploader/NewUploader";
 import Profile from "../Profile/Profile";
 import Webcami from "../Webcam/Webcam";
 
-function TaskModal(props){
+function TaskModal(props) {
 
     const customStyles = {
         content: {
             padding: '40px',
             maxWidth: '768px',
             width: '100%',
-            height:'auto',
+            height: 'auto',
             top: '55%',
             left: '50%',
             right: 'auto',
@@ -24,38 +24,53 @@ function TaskModal(props){
             flexDirection: 'column',
             alignItems: 'center',
             justifyContent: 'center',
-            background:'#FFFFFF',
-            borderRadius:'20px',
+            background: '#FFFFFF',
+            borderRadius: '20px',
         },
         overlay: {zIndex: 1000}
     };
+    const [openCamera, setOpenCamera] = useState(false)
+    const [takeImg, setTakeImg] = useState('')
 
-
-    return(
+    return (
         <>
             <Modal
                 isOpen={props?.tasks[props?.indx] !== undefined}
-                onRequestClose={()=>props?.onCloseTaskModal()}
+                onRequestClose={() => props?.onCloseTaskModal()}
                 style={customStyles}
                 ariaHideApp={false}
                 // className="YouClass"
             >
                 <div className={"modalInside"}>
                     <div className={"closingModal"}
-                         onClick={()=>props?.onCloseTaskModal()}
+                         onClick={() => props?.onCloseTaskModal()}
                     >
-                        <img className={"close"} src={Close} alt="" />
+                        <img className={"close"} src={Close} alt=""/>
                     </div>
                     <div>
                         <div className={"modalTitle"}>{props?.tasks[props?.indx]?.taskName}</div>
                         <div className={"modalText"}>{props?.tasks[props?.indx]?.taskText}</div>
                         <div className={"buttonsDiv"}>
-                            {/*<div className={"leftButton"}>Take a photo</div>*/}
-                            {/*<Profile/>*/}
-                            <Webcami/>
+                            {
+                                openCamera ?
+                                    <Webcami setOpenCamera={setOpenCamera} setTakeImg={setTakeImg}/>
+                                    :
+                                    <div className={"leftButton"} onClick={() => setOpenCamera(true)}>Take a photo</div>
+
+                            }
+                            {/*   <Profile/>*/}
                             <NewUploader uploadImages={props.uploadImages} indx={props.indx}
-                            tasks={props.tasks}/>
+                                         tasks={props.tasks}/>
                         </div>
+                        {takeImg ?
+                            <img
+                                src={takeImg}
+                                alt="Scan"
+                                style={{width: "100px", height: "auto"}}
+                            />
+                            :
+                            null
+                        }
                     </div>
                 </div>
             </Modal>
--- src/app/components/Webcam/Webcam.js
+++ src/app/components/Webcam/Webcam.js
@@ -1,21 +1,35 @@
+/*
 import React, { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 import './Webcam.style.css'
 
+
+const FACING_MODE_USER = "user";
+const FACING_MODE_ENVIRONMENT = "environment";
+
 const WebCami = () => {
     const webcamRef = useRef(null);
     const [capturedImage, setCapturedImage] = useState(null);
+    const [facingMode, setFacingMode] = useState(FACING_MODE_USER);
 
     const captureImage = () => {
         const imageSrc = webcamRef.current.getScreenshot();
         setCapturedImage(imageSrc);
     };
 
+    const handleClick = React.useCallback(() => {
+        setFacingMode((prevState) =>
+            prevState === FACING_MODE_USER
+                ? FACING_MODE_ENVIRONMENT
+                : FACING_MODE_USER
+        );
+    }, []);
+
     return (
         <div>
             <div className={"leftButton"} onClick={captureImage}>Take a photo</div>
-            {/*<button onClick={captureImage}>Capture</button>*/}
             <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+            <button onClick={handleClick}>Switch camera</button>
             {capturedImage && (
                 <img src={capturedImage} alt="Captured" width="300" height="200" />
             )}
@@ -62,10 +76,76 @@ export default WebCami;
 //         <div>
 //             <Webcam audio={false} ref={webcamRef} />
 //             <button onClick={captureImage}>Capture Image</button>
-//             {/* Add capture button and other UI elements */}
+//             {/!* Add capture button and other UI elements *!/}
 //         </div>
 //     );
 // };
 //
 //
 // export default Webcami;
+*/
+
+
+
+import React, { useState } from "react";
+import Webcam from "react-webcam";
+
+const FACING_MODE_USER = "user";
+const FACING_MODE_ENVIRONMENT = "environment";
+
+export default function WebCami({setOpenCamera, setTakeImg}) {
+    const webcamRef = React.useRef(null);
+    const [image, setImage] = useState("");
+
+    const [facingMode, setFacingMode] = React.useState(FACING_MODE_USER);
+
+    const capture = React.useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        //setImage(imageSrc);
+        setTakeImg(imageSrc);
+        setOpenCamera(false);
+    }, [webcamRef]);
+
+    let videoConstraints: MediaTrackConstraints = {
+        facingMode: facingMode,
+        width: 270,
+        height: 480
+    };
+
+    const handleClick = React.useCallback(() => {
+        setFacingMode((prevState) =>
+            prevState === FACING_MODE_USER
+                ? FACING_MODE_ENVIRONMENT
+                : FACING_MODE_USER
+        );
+    }, []);
+
+    console.log(facingMode + videoConstraints);
+
+    return (
+        <>
+            <div className="webcam-container">
+                <div className="webcam-img">
+                  {/*  {image === "" ? (*/}
+                        <Webcam
+                            className="webcam"
+                            audio={false}
+                            ref={webcamRef}
+                            screenshotFormat="image/jpeg"
+                            videoConstraints={videoConstraints}
+                            screenshotQuality={1}
+                        />
+                 {/*   ) : (
+                        <img
+                            src={image}
+                            alt="Scan"
+                            style={{ width: "500px", height: "auto" }}
+                        />
+                    )}*/}
+                </div>
+                <button onClick={handleClick}>Switch camera</button>
+                <button onClick={capture}>capture</button>
+            </div>
+        </>
+    );
+}
